d942151ff70ccff9fef4febe2383a1c7701213cb
youzhen <yl301114@gmail.com>
2023-06-27 02:39:51 +0800

    email

--- public/js/index.js
+++ public/js/index.js
@@ -88,3 +88,19 @@ var forestSwiper = new Swiper(".forest_swiper", {
 $(".btn-top").click(function(){
     $("html, body").animate({scrollTop: 0},300);
 })
+
+function sendEmail(){
+    Email.send({
+        SecureToken: "8207c7d7-4a6c-4797-870d-d16ee71023ce",
+        To : "yl301114@gmail.com",
+        From : "walayydesign@gmail.com",
+        Subject : "昀真馥田預約賞屋",
+        Body : "姓名:" + document.getElementById("name").value
+                + "<br>電話:" + document.getElementById("phone").value
+                + "<br>可聯繫時間:" + document.getElementById("time").value
+                + "<br>信箱:" + document.getElementById("email").value
+                + "<br>留言:" + document.getElementById("message").value
+    }).then(
+        message => alert("感謝您的來信！我們很快就會和您聯繫！"),
+    );
+}
